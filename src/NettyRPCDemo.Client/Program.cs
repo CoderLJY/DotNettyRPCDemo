7caf85e33a756bea89b3f101ac42a7bd4623e675
linsi <jianyue.lin@huoshen.info>
2019-02-12 16:32:00 +0800

    first commit, can successfully run

---
+++ src/NettyRPCDemo.Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using DotNetty.Codecs;
+using DotNetty.Handlers.Logging;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Embedded;
+using DotNetty.Transport.Channels.Sockets;
+
+namespace NettyRPCDemo.Client
+{
+    class Program
+    {
+        static async Task RunClientAsync()
+        {
+//            ExampleHelper.SetConsoleLogger();
+
+            var group = new MultithreadEventLoopGroup();
+
+//            X509Certificate2 cert = null;
+//            string targetHost = null;
+//            if (ClientSettings.IsSsl)
+//            {
+//                cert = new X509Certificate2(Path.Combine(ExampleHelper.ProcessDirectory, "dotnetty.com.pfx"), "password");
+//                targetHost = cert.GetNameInfo(X509NameType.DnsName, false);
+//            }
+            try
+            {
+                var bootstrap = new Bootstrap();
+                bootstrap
+                    .Group(group)
+                    .Channel<TcpSocketChannel>()
+                    .Option(ChannelOption.TcpNodelay, true)
+                    .Option(ChannelOption.SoKeepalive, true)
+                    .Handler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+
+//                        if (cert != null)
+//                        {
+//                            pipeline.AddLast("tls", new TlsHandler(stream => new SslStream(stream, true, (sender, certificate, chain, errors) => true), new ClientTlsSettings(targetHost)));
+//                        }
+                        pipeline.AddLast(new LoggingHandler());
+                        pipeline.AddLast("framing-enc", new LengthFieldPrepender(2));
+                        pipeline.AddLast("framing-dec", new LengthFieldBasedFrameDecoder(ushort.MaxValue, 0, 2, 0, 2));
+                        pipeline.AddLast("echo", new RPCDemoClientHandler());
+                    }));
+
+                IChannel clientChannel = await bootstrap.ConnectAsync(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8007));
+
+                Console.ReadLine();
+
+                await clientChannel.CloseAsync();
+            }
+            finally
+            {
+                await group.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1));
+            }
+        }
+
+        static void Main() => RunClientAsync().Wait();
+    }
+}
